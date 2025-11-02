ee3928717fe4106de112eb28989a426c0952a692
NAGASHREE K S <naagashree42@gmail.com>
2025-11-02 12:42:26 +0530

    Create searchInStrings.java

---
+++ src/searchInStrings.java
@@ -0,0 +1,15 @@
+public static boolean searchString(String str,char target)
+	{
+		if(str.length()==0)
+		{
+			return false;
+		}
+		for(int i=0;i<str.length();i++)
+		{
+			if(target==str.charAt(i))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
