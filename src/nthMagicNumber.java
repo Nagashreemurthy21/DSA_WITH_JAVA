71fcd958b253a4ec79ab4168963ab0fda1a0d78e
NAGASHREE K S <naagashree42@gmail.com>
2025-11-05 20:45:22 +0530

    Create nthMagicNumber.java

---
+++ src/nthMagicNumber.java
@@ -0,0 +1,13 @@
+public static int nthMagicNumber(int n)
+	{
+		int ans=0;
+		int base=5;
+		while(n>0)
+		{	
+			int last=(n&1);
+			n=n>>1;
+			ans+=last*base;
+			base=base*5;
+		}
+		return ans;
+	}
