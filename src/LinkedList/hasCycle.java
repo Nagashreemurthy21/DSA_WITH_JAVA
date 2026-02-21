79709904a8096be3bc38839d6320492b965721af
NAGASHREE K S <naagashree42@gmail.com>
2026-02-21 22:16:50 +0530

    Create hasCycle.java

---
+++ src/LinkedList/hasCycle.java
@@ -0,0 +1,15 @@
+public boolean hasCycle(Node head)
+	{
+		Node fast=head;
+		Node slow=head;
+		while(fast!=null && fast.next!=null)
+		{
+			fast=fast.next.next;
+			slow=slow.next;
+			if(fast==slow)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
