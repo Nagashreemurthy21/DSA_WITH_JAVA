6c2cf07726be362699e9df5575307d908e266f5a
NAGASHREE K S <naagashree42@gmail.com>
2026-02-26 18:54:27 +0530

    Update patterns.java

--- src/patterns.java
+++ src/patterns.java
@@ -168,3 +168,65 @@ int n=sc.nextInt();
 			}
 			System.out.println();
 		}
+
+//number pyramid//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+
+//palindromic pattern//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=i;j>=1;j--)
+			{
+				System.out.print(j);
+			}
+			for(int j=2;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+
+//diamond pattern//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=n-1;i>=1;i--)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
