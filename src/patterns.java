672e7be6106d85a50de060386415a9405307a532
NAGASHREE K S <naagashree42@gmail.com>
2026-02-26 18:35:40 +0530

    Update patterns.java

--- src/patterns.java
+++ src/patterns.java
@@ -26,3 +26,145 @@ int n=sc.nextInt();
 			}
 			System.out.println();
 		}
+
+//half pyramid//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+
+//inverted half pyramid//
+int n=sc.nextInt();
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+//inverted half pyramid rotated by 180 degree//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+//half pyramid with numbers//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+//inverted half pyramid with numbers//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i+1;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+//floyd's triangle//
+int n=sc.nextInt();
+		int num=1;
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print(num+" ");
+				num++;
+			}
+			System.out.println();
+		}
+
+//0-1 triangle//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				int sum=i+j;
+				if(sum%2==0)
+				{
+					System.out.print("0 ");
+				}
+				else
+				{
+					System.out.print("1 ");
+				}
+			}
+			System.out.println();
+		}
+
+//butterfly pattern//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			for(int j=1;j<=2*(n-i);j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=n;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			for(int j=1;j<=2*(n-i);j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+//solid rhombus//
+int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n-i;j++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=n;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
